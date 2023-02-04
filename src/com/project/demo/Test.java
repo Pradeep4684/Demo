71533022339d311ac101d3c79760760123c82d0f
Pradeep4684 <pradeep.andhale97@gmail.com>
2023-02-04 15:39:44 +0530

    First commit : Added arraylsit democode

---
+++ src/com/project/demo/Test.java
@@ -0,0 +1,17 @@
+package com.project.demo;
+
+import java.util.LinkedList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(10);
+		list.add(15);
+		list.add(20);
+		list.add(25);
+		
+		System.out.println(list);
+		
+	}
+}
